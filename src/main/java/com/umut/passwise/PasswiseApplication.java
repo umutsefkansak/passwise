3448a09f20fa74c556ccbfccaaa2a01e10097482
umut <umut.sefkan4@gmail.com>
2025-03-25 11:12:33 +0300

    İlk commit

---
+++ src/main/java/com/umut/passwise/PasswiseApplication.java
@@ -0,0 +1,13 @@
+package com.umut.passwise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umut/passwise/PasswiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umut.passwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
