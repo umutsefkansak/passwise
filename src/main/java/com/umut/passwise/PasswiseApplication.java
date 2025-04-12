bc509589194d62856a12380fbc6a41f57da95be4
umutsefkansak <umut.sefkan4@gmail.com>
2025-04-12 03:28:31 +0300

    gereksiz kod silindi

--- src/main/java/com/umut/passwise/PasswiseApplication.java
+++ src/main/java/com/umut/passwise/PasswiseApplication.java
@@ -9,13 +9,6 @@ public class PasswiseApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(PasswiseApplication.class, args);
-
-		System.out.println("----------------------------------------------------------------");
-		for(int i = 1; i<3; i++){
-			System.out.println(i);
-		}
-		System.out.println("----------------------------------------------------------------");
-
 	}
 
 }
